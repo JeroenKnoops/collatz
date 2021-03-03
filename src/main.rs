51fc73a506aafb09bbd3cfff2a8123ff783cfded
Jeroen Knoops <jeroen.knoops@philips.com>
2021-03-03 11:07:23 +0100

    cargo clippy and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn calculate_number(input: isize) -> isize {
     match (input % 2) == 0 {
         true => input / 2,
-        false => input * 3 + 1
+        false => input * 3 + 1,
     }
 }
 
 fn main() {
     let matches = App::new("collatz")
-                          .version("v0.1")
-                          .author("Jeroen Knoops <jeroen.knoops@gmail.com>")
-                          .about("calculates collatz number given an input")
-                          .arg(Arg::with_name("NUMBER")
-                               .help("Sets number for which the collatz number is calculated... guess the answer ;)")
-                               .required(true)
-                               .index(1))
-                          .get_matches();
+        .version("v0.1")
+        .author("Jeroen Knoops <jeroen.knoops@gmail.com>")
+        .about("calculates collatz number given an input")
+        .arg(
+            Arg::with_name("NUMBER")
+                .help(
+                    "Sets number for which the collatz number is calculated... guess the answer ;)",
+                )
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
 
-    let number = matches.value_of("NUMBER").unwrap().parse::<isize>().unwrap();
+    let number = matches
+        .value_of("NUMBER")
+        .unwrap()
+        .parse::<isize>()
+        .unwrap();
     println!("Using number: {}", number);
     let mut result = number;
     let mut age = 0;
@@ -28,5 +36,4 @@ fn main() {
         result = calculate_number(result);
         println!("Result: {}: {}", age, result);
     }
-
 }
